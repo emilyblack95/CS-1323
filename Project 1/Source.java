c243b5cd54341a4ff93bc0fab9a8a9242efd9ef8
Emily Black <emily.2014@yahoo.com>
2016-12-21 01:27:41 -0600

    Create Source.java

---
+++ Project 1/Source.java
@@ -0,0 +1,14 @@
+/** This program prints out fun facts about CS.
+* @author Emily C. Black
+* @version 1.0
+*/
+public class CSFacts
+{
+	//This is the main program. The instructions here
+	// are what is done when the program is run
+	public static void main(String [] args)   //extra credit points for mentioning this? its the main method
+	{
+		System.out.println("Future opportunities in computing are without boundaries.");
+	}
+	
+}
